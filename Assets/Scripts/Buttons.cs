4ee1b3fb214de38d653fcf0ac6706839bc547100
StrikingLetsPlays <007makiz@gmail.com>
2020-07-21 19:27:21 +0300

    UI Eddits!

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Buttons : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D PlayerRB;
+    public void Jump()
+    {
+        CrossPlatformInputManager.SetButtonDown("Jump");
+        CrossPlatformInputManager.SetButtonUp("Jump");
+    }
+    public void WeightDown()
+    {
+        PlayerRB.gravityScale = 5f;
+    }
+    public void WeightUp()
+    {
+        PlayerRB.gravityScale = 3f;
+    }
+}
--- Assets/Standard Assets/CrossPlatformInput/Scripts/Joystick.cs
+++ Assets/Standard Assets/CrossPlatformInput/Scripts/Joystick.cs
@@ -18,13 +18,11 @@ namespace UnityStandardAssets.CrossPlatformInput
 		}
 
 		public int MovementRange = 100;
-		public AxisOption axesToUse = AxisOption.Both; // The options for the axes that the still will use
+		public AxisOption axesToUse = AxisOption.OnlyHorizontal; // The options for the axes that the still will use
 		public string horizontalAxisName = "Horizontal"; // The name given to the horizontal axis for the cross platform input
-		public string verticalAxisName = "Vertical"; // The name given to the vertical axis for the cross platform input
 
 		Vector3 m_StartPos;
 		bool m_UseX; // Toggle for using the x axis
-		bool m_UseY; // Toggle for using the Y axis
 		CrossPlatformInputManager.VirtualAxis m_HorizontalVirtualAxis; // Reference to the joystick in the cross platform input
 		CrossPlatformInputManager.VirtualAxis m_VerticalVirtualAxis; // Reference to the joystick in the cross platform input
 
@@ -47,29 +45,18 @@ namespace UnityStandardAssets.CrossPlatformInput
 			{
 				m_HorizontalVirtualAxis.Update(-delta.x);
 			}
-
-			if (m_UseY)
-			{
-				m_VerticalVirtualAxis.Update(delta.y);
-			}
 		}
 
 		void CreateVirtualAxes()
 		{
 			// set axes to use
 			m_UseX = (axesToUse == AxisOption.Both || axesToUse == AxisOption.OnlyHorizontal);
-			m_UseY = (axesToUse == AxisOption.Both || axesToUse == AxisOption.OnlyVertical);
 
 			// create new axes based on axes to use
 			if (m_UseX)
 			{
 				m_HorizontalVirtualAxis = new CrossPlatformInputManager.VirtualAxis(horizontalAxisName);
 				CrossPlatformInputManager.RegisterVirtualAxis(m_HorizontalVirtualAxis);
-			}
-			if (m_UseY)
-			{
-				m_VerticalVirtualAxis = new CrossPlatformInputManager.VirtualAxis(verticalAxisName);
-				CrossPlatformInputManager.RegisterVirtualAxis(m_VerticalVirtualAxis);
 			}
 		}
 
@@ -85,30 +72,6 @@ namespace UnityStandardAssets.CrossPlatformInput
 				newPos.x = delta;
 			}
 
-			if (m_UseY)
-			{
-				int delta = (int)(data.position.y - m_StartPos.y);
-				delta = Mathf.Clamp(delta, -MovementRange, MovementRange);
-				newPos.y = delta;
-				if (newPos.y > 40)
-				{
-					CrossPlatformInputManager.SetButtonDown("Jump");
-				}
-				else if(newPos.y <= 40)
-				{
-					CrossPlatformInputManager.SetButtonUp("Jump");
-				}
-				if (newPos.y < -40)
-				{
-					//increase weight
-					PlayerRB.gravityScale = 5f;
-				}
-				else if (newPos.y >= -40)
-				{
-					//decrease weight
-					PlayerRB.gravityScale = 3f;
-				}
-			}
 			transform.position = new Vector3(m_StartPos.x + newPos.x, m_StartPos.y + newPos.y, m_StartPos.z + newPos.z);
 			UpdateVirtualAxes(transform.position);
 		}
@@ -129,10 +92,6 @@ namespace UnityStandardAssets.CrossPlatformInput
 			if (m_UseX)
 			{
 				m_HorizontalVirtualAxis.Remove();
-			}
-			if (m_UseY)
-			{
-				m_VerticalVirtualAxis.Remove();
 			}
 		}
 	}
